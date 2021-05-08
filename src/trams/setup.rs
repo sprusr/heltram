d4f3a14620210e6a22f8714ce82cf1df60f92d40
sprusr <scott@spru.sr>
2021-05-08 13:11:11 +0300

    Improve modelling of Trams etc

--- src/trams/setup.rs
+++ src/trams/setup.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-pub(super) fn setup_materials(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+pub(super) fn setup_materials(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.insert_resource(super::types::Materials {
         stop_material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
         tram_material: materials.add(Color::rgb(0.1, 0.7, 0.1).into()),
@@ -9,18 +12,15 @@ pub(super) fn setup_materials(mut commands: Commands, mut materials: ResMut<Asse
 
 pub(super) fn setup_lines(mut commands: Commands, materials: Res<super::types::Materials>) {
     let stop1 = commands
-        .spawn_bundle(super::types::StopBundle::new(50.0, 10.0, &materials))
+        .spawn_bundle(super::types::TramStopBundle::new(50.0, 10.0, &materials))
         .id();
     let stop2 = commands
-        .spawn_bundle(super::types::StopBundle::new(20.0, -30.0, &materials))
+        .spawn_bundle(super::types::TramStopBundle::new(20.0, -30.0, &materials))
         .id();
-    let line = commands
+    let route = commands
         .spawn()
-        .insert(super::types::TramLine {
+        .insert(super::types::TramRoute {
             stops: vec![stop1, stop2],
         })
         .id();
-    commands.spawn_bundle(super::types::TramBundle::new(line, 20.0, 20.0, &materials));
-    commands.spawn_bundle(super::types::TramBundle::new(line, 0.0, 0.0, &materials));
-    commands.spawn_bundle(super::types::TramBundle::new(line, -20.0, -20.0, &materials));
 }
--- src/trams/types.rs
+++ src/trams/types.rs
@@ -5,24 +5,47 @@ pub(super) struct Materials {
     pub(super) tram_material: Handle<ColorMaterial>,
 }
 
+/// Segment of track in a single direction
 pub(crate) struct TramLine {
+    /// One end of the TramLine segment, connecting entities with TramStop or
+    /// TramLine components. Empty means end of line with no loop.
+    pub(super) from: Vec<Entity>,
+    /// Other end of the TramLine segment, connecting entities with TramStop or
+    /// TramLine components. Empty means end of line with no loop.
+    pub(super) to: Vec<Entity>,
+}
+
+/// Route of TramStops which Trams can run on
+pub(crate) struct TramRoute {
+    /// Entities with TramStop components which this TramRoute consists of
     pub(super) stops: Vec<Entity>,
 }
 
-pub(crate) struct TramStop;
+/// When visited by a TramRoute, Trams can stop here
+pub(crate) struct TramStop {
+    /// Entities with TramLine component which come into this TramStop. Empty
+    /// means end of line with no loop.
+    pub(super) incoming: Vec<Entity>,
+    /// Entities with TramLine component which go out of this TramStop. Empty
+    /// means end of line with no loop.
+    pub(super) outgoing: Vec<Entity>,
+}
 
 #[derive(Bundle)]
-pub(super) struct StopBundle {
+pub(super) struct TramStopBundle {
     pub(super) stop: TramStop,
 
     #[bundle]
     sprite: SpriteBundle,
 }
 
-impl StopBundle {
+impl TramStopBundle {
     pub(super) fn new(x: f32, y: f32, materials: &Res<Materials>) -> Self {
-        StopBundle {
-            stop: TramStop {},
+        TramStopBundle {
+            stop: TramStop {
+                incoming: vec![],
+                outgoing: vec![],
+            },
             sprite: SpriteBundle {
                 material: materials.stop_material.clone(),
                 sprite: Sprite::new(Vec2::new(10.0, 10.0)),
@@ -37,7 +60,12 @@ impl StopBundle {
 }
 
 pub(crate) struct Tram {
-    pub(super) line: Entity,
+    /// Entity with TramRoute component which this Tram is currently following
+    pub(super) route: Entity,
+    /// Entity with TramLine or TramStop component which this Tram is on/at
+    pub(super) location: Entity,
+    /// Progress along TramLine segment if applicable
+    pub(super) progress: Option<f64>,
 }
 
 #[derive(Bundle)]
@@ -49,16 +77,16 @@ pub(super) struct TramBundle {
 }
 
 impl TramBundle {
-    pub(super) fn new(line: Entity, x: f32, y: f32, materials: &Res<Materials>) -> Self {
+    pub(super) fn new(route: Entity, location: Entity, materials: &Res<Materials>) -> Self {
         TramBundle {
-            tram: Tram { line },
+            tram: Tram {
+                route,
+                location,
+                progress: None,
+            },
             sprite: SpriteBundle {
                 material: materials.tram_material.clone(),
                 sprite: Sprite::new(Vec2::new(10.0, 10.0)),
-                transform: Transform {
-                    translation: Vec3::new(x, y, 0.0),
-                    ..Default::default()
-                },
                 ..Default::default()
             },
         }
